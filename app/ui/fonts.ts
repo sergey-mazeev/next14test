51499a26b25309a5f79ed10caafc59b0ec32f1e4
Sergey Mazeev <sergey.mazeev@gmail.com>
2023-10-31 06:36:46 +0500

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter, Lusitana} from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin', 'cyrillic']});
+
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']});
